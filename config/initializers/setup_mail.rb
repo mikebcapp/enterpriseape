574b601f862806772a98557708852d4d8524f867
Mike Capper <mikebcapp@gmail.com>
2014-10-29 08:25:02 +0000

    Added sign in, sign out & sign up functionality

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => 'smtp.sendgrid.net',
   :port                 => '587',
   :authentication       => :plain,
-  :user_name            => 'app30818038@heroku.com'
-  :password             => 'cgklferc'
+  :user_name            => 'app30818038@heroku.com',
+  :password             => 'cgklferc',
   :domain                => 'heroku.com',
   :enable_starttls_auto  => true
   }
